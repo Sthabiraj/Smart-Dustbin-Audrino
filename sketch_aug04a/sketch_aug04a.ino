4bbf7cf526f13ad7ae149ca339e96cfe60b4fdf6
Sthabiraj <birajshrestha2007@gmail.com>
2023-08-09 21:56:29 +0545

    Added arduino code

--- sketch_aug04a/sketch_aug04a.ino
+++ sketch_aug04a/sketch_aug04a.ino
@@ -1,13 +1,70 @@
-void setup() {
-  Serial.begin(9600); // Set the baud rate to match the baud rate in the Python script
+
+
+// Distance Sensor
+// For outside sensor
+int trigPin1 = 2;
+int echoPin1 = 3;
+// For inside sensor
+int trigPin2 = 4;
+int echoPin2 = 5;
+
+// For Servo
+#include <Servo.h>
+
+int count = 0;
+
+Servo myServo;
+int servoPosition = 9;
+
+void setup(){
+  // For Distance Sensor
+  // For outside sensor
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  // For inside sensor
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  // For Servo
+  myServo.attach(servoPosition);
+  myServo.write(0);
+  // Serial Monitor
+  Serial.begin(9600);
 }
 
-void loop() {
-  // Read data from your sensor or device
-  char sensorData[20] = "Hello"; // Replace this with your actual sensor reading.
-
-  // Send the data through the Serial interface
-  Serial.println(sensorData);
-
-  delay(1000); // Adjust the delay based on how often you want to send data (1 second in this example)
+void loop(){
+  // For outside sensor
+  digitalWrite(trigPin1, LOW);
+  delay(2);
+  digitalWrite(trigPin1, HIGH);
+  delay(10); 
+  digitalWrite(trigPin1, LOW);
+  // Calculate distance1
+  float time1 = pulseIn(echoPin1, HIGH);
+  float distance1 = (0.0343 * time1)/2;
+  // Open the dustbin lid if person is near
+  if(distance1 <= 30){
+    myServo.write(120);
+  }
+  else{
+    myServo.write(0);
+  }
+  
+  
+  // For inside sensor
+  digitalWrite(trigPin2, LOW);
+  delay(2);
+  digitalWrite(trigPin2, HIGH);
+  delay(10); 
+  digitalWrite(trigPin2, LOW);
+  // Calculate distance2
+  float time2 = pulseIn(echoPin2, HIGH);
+  float distance2 = (0.0343 * time2)/2;
+  // Alert user if dustbin is full
+  if(distance2 <= 3 && count == 0){
+    Serial.println("Your dustbin is full! Please empty it.");
+    count++;
+  }
+  else{
+    count--;
+  }
 }
