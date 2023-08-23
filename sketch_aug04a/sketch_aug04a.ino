69938e2579b9162114e71541c723979e406f1793
Sthabiraj <birajshrestha2007@gmail.com>
2023-08-23 08:20:28 +0545

    code completed

--- sketch_aug04a/sketch_aug04a.ino
+++ sketch_aug04a/sketch_aug04a.ino
@@ -14,9 +14,6 @@ int servoPosition = 9;
 // Flag if the dustbin is full
 bool isDustbinFull = false;
 
-// Flag if the lid is open
-bool lidOpen = false;
-
 // Location name
 char locationName[100] = "Herald College Kathmandu";
 
@@ -61,37 +58,34 @@ void loop()
   else
   {
     myServo.write(0);
+    checkDustbin();
   }
+}
 
-  // For inside sensor
-  if (lidOpen)
-  {
-    digitalWrite(trigPin2, LOW);
-    delay(2);
-    digitalWrite(trigPin2, HIGH);
-    delay(10);
-    digitalWrite(trigPin2, LOW);
-  }
-  else
-  {
-    digitalWrite(trigPin2, LOW);
-  }
+void checkDustbin()
+{
+  digitalWrite(trigPin2, LOW);
+  delay(2);
+  digitalWrite(trigPin2, HIGH);
+  delay(10);
+  digitalWrite(trigPin2, LOW);
 
   // Calculate distance2
   float time2 = pulseIn(echoPin2, HIGH);
   float distance2 = (0.0343 * time2) / 2;
 
   // Check if dustbin is full and flag isDustbin to true
-  if (distance2 <= 5 && !isDustbinFull)
+  if (distance2 <= 8 && !isDustbinFull)
   {
     Serial.print("Your dustbin at ");
     Serial.print(locationName);
     Serial.println(" is full! Please empty it.");
+    delay(3000);
     isDustbinFull = true;
   }
 
   // Flag isDustbinFull to false if the dustbin is not full
-  if (distance2 > 5)
+  if (distance2 > 8)
   {
     isDustbinFull = false;
   }
