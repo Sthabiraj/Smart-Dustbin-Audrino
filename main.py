a6cc556ea7d9c7c6ce5bd6aafbcf96406f20d012
Sthabiraj <birajshrestha2007@gmail.com>
2023-08-25 16:21:14 +0545

    Commented python code

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Student name: Biraj Shrestha
-# Project title: Smart Dustbin
+# Project title: Smart Dustbin - Serial to Google Chat
 # Date: August 25, 2023
 
-import serial
-import requests
+import serial  # Import the serial library for communication with Arduino
+import requests  # Import the requests library for sending HTTP requests
 
 # Set the serial port and baud rate for your Arduino
 serial_port = 'COM3'  # Replace 'COM3' with the appropriate port for your Arduino
 baud_rate = 9600
 
 # Google Chat Webhook URL
-webhook_url = 'https://chat.googleapis.com/v1/spaces/AAAAU6itNNA/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=S7hB3cEhxA-H2Tag2P3HYfBGoX-sAGyRwc0noD687i4'
+webhook_url = 'https://chat.googleapis.com/v1/spaces/AAAAU6itNNA/messages?key=your_key&token=your_token'
 
 # Open the serial connection to the Arduino
 ser = serial.Serial(serial_port, baud_rate)
@@ -28,4 +28,5 @@ while True:
     # Send the data to Google Chat via HTTP POST request
     response = requests.post(webhook_url, json=payload)
 
+    # Print the response status code for confirmation
     print(f"Data sent to Google Chat. Response: {response.status_code}")
